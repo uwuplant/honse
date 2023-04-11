804aa2ed73cd9314ae4a6831c7242b8c98fbe6dd
h <h>
2023-04-11 17:11:11 -0400

    wip pesto tables

--- src/search/eval.rs
+++ src/search/eval.rs
@@ -1,16 +1,53 @@
 use cozy_chess::{Board, Color, Piece};
-const PIECE_VALUES: [i16; 6] = [100, 320, 330, 500, 900, 0];
+use once_cell::sync::Lazy;
+use super::evaluation::piece_square_table::{*};
+
+pub const MG_PIECE_VALUES: [i16; 6] = [82, 337, 365, 477, 1025, 0];
+pub const EG_PIECE_VALUES: [i16; 6] = [94, 281, 297, 512, 936, 0];
+pub const PHASE_VALUES: [i16; 6] = [0, 1, 1, 2, 4, 0];
+static PST: Lazy<PieceSquareTable> = Lazy::new(PieceSquareTable::new);
+
+fn piece_type(piece: Piece) -> usize {
+    match piece {
+        Piece::Pawn => 0,
+        Piece::Knight => 1,
+        Piece::Bishop => 2,
+        Piece::Rook => 3,
+        Piece::Queen => 4,
+        Piece::King => 5,
+    }
+}
 
 pub fn eval(board: &Board) -> i16 {
     let mut score = 0;
 
-    for piece in Piece::ALL {
-        score +=
-            PIECE_VALUES[piece as usize] * board.colored_pieces(Color::White, piece).len() as i16;
+    let mut mg = 0;
+    let mut eg = 0;
+    let mut game_phase = 0;
 
-        score -=
-            PIECE_VALUES[piece as usize] * board.colored_pieces(Color::Black, piece).len() as i16;
+    for square in board.occupied() {
+        let piece = board.piece_on(square).unwrap();
+        let color = board.color_on(square).unwrap();
+        let sq = square as usize;
+
+        game_phase += PHASE_VALUES[piece_type(piece)];
+
+        match color {
+            Color::White => {
+                mg += PST.mg_table[piece_type(piece)][sq];
+                eg += PST.eg_table[piece_type(piece)][sq];
+            }
+            Color::Black => {
+                mg -= PST.mg_table[piece_type(piece) + 6][sq];
+                eg -= PST.eg_table[piece_type(piece) + 6][sq];
+            }
+        }
     }
+
+    let mg_weight = game_phase.min(24);
+    let eg_weight = 24 - mg_weight;
+
+    score += (((mg * mg_weight) + (eg * eg_weight)) / 24) as i16;
 
     match board.side_to_move() {
         Color::White => score,
@@ -29,7 +66,7 @@ mod test {
             "rnb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
             false,
         )
-        .unwrap();
+            .unwrap();
 
         assert!(eval(&board) > 0)
     }
---
+++ src/search/evaluation/mod.rs
@@ -0,0 +1,1 @@
+pub mod piece_square_table;
